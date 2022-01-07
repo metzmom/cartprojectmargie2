5cfa7a169474c8f57d6a52516bb501ccddda70d3
metzmom <84488543+metzmom@users.noreply.github.com>
2022-01-07 15:14:28 -0600

    complete POST ADD part 1

--- cartprojectmargie2/src/main/java/com/metzm/cartprojectmargie2/controllers/AdminPagesController.java
+++ cartprojectmargie2/src/main/java/com/metzm/cartprojectmargie2/controllers/AdminPagesController.java
@@ -6,9 +6,13 @@ import com.metzm.cartprojectmargie2.models.data.PageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -44,6 +48,39 @@ public class AdminPagesController {
 
         model.addAttribute("page", new Page());
         return "admin/pages/add";
+    }
+
+
+//to redirect for errors
+
+    @PostMapping("/add")
+    public String add (@Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model){
+        if (bindingResult.hasErrors()) {
+            return "admin/pages/add";  //contains error messages
+        }
+
+        redirectAttributes.addFlashAttribute("message", "PAGE added");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+// this will enable the comparison of slug variable. if it exists change to lower case and replace spaces with -.
+        String slug = page.getSlug() =="" ? page.getTitle().toLowerCase().replace(" ", "-") : page.getSlug().toLowerCase().replace(" ","-");
+
+
+   // this will check to make sure only one slug exists because they are URL
+        Page slugExists = pageRepo.findBySlug(slug);
+
+        if ( slugExists != null) {
+//if slug doesnt exist  danger
+            redirectAttributes.addFlashAttribute("message", "slug exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+        } else {
+            page.setSlug(slug);
+            page.setSorting(100);//sorts to last page of data
+
+            pageRepo.save(page);//save the changes
+
+        }
+        return "redirect:/admin/pages/add"; //return back to the page
     }
 
 }
--- cartprojectmargie2/src/main/java/com/metzm/cartprojectmargie2/models/data/PageRepository.java
+++ cartprojectmargie2/src/main/java/com/metzm/cartprojectmargie2/models/data/PageRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PageRepository extends JpaRepository<Page,Integer> {
 
+        Page findBySlug(String slug);
 
 
 //    @Override
