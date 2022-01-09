cc64ee92febd548166dbae94b4731ec3880e78f9
metzmom <84488543+metzmom@users.noreply.github.com>
2022-01-08 23:54:17 -0600

    create products SQL table entity and repository

---
+++ cartprojectmargie2/src/main/java/com/metzm/cartprojectmargie2/models/data/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.metzm.cartprojectmargie2.models.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository  extends JpaRepository<Product,Integer> {
+
+
+
+}
