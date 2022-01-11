632f8dd5c4c1d075aec8e282ca5449d53a7e0ef0
metzmom <84488543+metzmom@users.noreply.github.com>
2022-01-10 20:30:41 -0600

    working for product pages and categores..no images render yet

--- cartprojectmargie2/src/main/java/com/metzm/cartprojectmargie2/models/data/ProductRepository.java
+++ cartprojectmargie2/src/main/java/com/metzm/cartprojectmargie2/models/data/ProductRepository.java
@@ -1,9 +1,30 @@
 package com.metzm.cartprojectmargie2.models.data;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+/*import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository  extends JpaRepository<Product,Integer> {
 
 
+
+}*/
+//package com.vojislavk.cmsshoppingcart.models;
+
+        import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    Product findBySlug(String slug);
+
+    Product findBySlugAndIdNot(String slug, int id);
+
+    Page<Product> findAll(Pageable pagable);
+
+    List<Product> findAllByCategoryId(String categoryId, Pageable pageable);
+
+    long countByCategoryId(String categoryId);
 
 }
--- cartprojectmargie2/src/main/resources/static/js/app.js
+++ cartprojectmargie2/src/main/resources/static/js/app.js
@@ -4,4 +4,22 @@ $(function () {
         if (!confirm("Confirm deletion")) return false;
     });
 
- });
+    });
+
+  /*  if ( $("#content").length) {
+        ClassicEditor
+            .create(document.querySelector("#content"))
+            .catch(error => {
+                console.log(error);
+            });
+    }
+    function readURL(input, idNum) {
+        if (input.files && input.files[0]) {
+            let reader = new FileReader();
+
+            reader.onload = function (e) {
+                $("#imgPreview" + idNum).attr("src", e.target.result).width(100).height(100);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }*/
