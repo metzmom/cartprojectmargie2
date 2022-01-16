5804488437c9279005cac6cf09f25634d81b8fa5
metzmom <84488543+metzmom@users.noreply.github.com>
2022-01-15 21:18:48 -0600

    added cart view to home page

--- cartprojectmargie2/src/main/java/com/metzm/cartprojectmargie2/Common.java
+++ cartprojectmargie2/src/main/java/com/metzm/cartprojectmargie2/Common.java
@@ -2,6 +2,7 @@ package com.metzm.cartprojectmargie2;
 
 //this page goes before all other controller and will make links to pages by creating cpages variable
 
+import com.metzm.cartprojectmargie2.models.Cart;
 import com.metzm.cartprojectmargie2.models.CategoryRepository;
 import com.metzm.cartprojectmargie2.models.data.Category;
 import com.metzm.cartprojectmargie2.models.data.Page;
@@ -11,10 +12,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.List;
 
 @ControllerAdvice
-  //  @SuppressWarnings("unchecked")
+@SuppressWarnings("unchecked")
     public class Common {
 
     @Autowired
@@ -24,13 +27,35 @@ import java.util.List;
     private CategoryRepository categoryRepo;
 
     @ModelAttribute
-    public void sharedData(Model model) {
+    public void sharedData(Model model, HttpSession session) {
 
-         List<Page> pages = pageRepo.findAllByOrderBySortingAsc();
+        List<Page> pages = pageRepo.findAllByOrderBySortingAsc();
 
-         List<Category> categories = categoryRepo.findAll();
+        List<Category> categories = categoryRepo.findAll();
 
-        model.addAttribute("cpages",pages);
-        model.addAttribute("ccategories",categories);
+        boolean cartActive = false;
+
+        if (session.getAttribute("cart") != null) {
+
+            HashMap<Integer, Cart> cart = (HashMap<Integer, Cart>)session.getAttribute("cart");
+
+            int size = 0;
+            double total = 0;
+
+            for (Cart value : cart.values()) {
+                size += value.getQuantity();
+                total += value.getQuantity() * Double.parseDouble(value.getPrice());
+            }
+
+            model.addAttribute("csize", size);
+            model.addAttribute("ctotal", total);
+            cartActive = true;
+        }
+
+        model.addAttribute("cpages", pages);
+        model.addAttribute("ccategories", categories);
+        model.addAttribute("cartActive", cartActive);
     }
+
+
 }
