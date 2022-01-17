a1c808d870267e4a41437f84b7d6aff7cd132cfc
metzmom <84488543+metzmom@users.noreply.github.com>
2022-01-17 13:49:42 -0600

    user details added

--- cartprojectmargie2/src/main/java/com/metzm/cartprojectmargie2/security/RegistrationController.java
+++ cartprojectmargie2/src/main/java/com/metzm/cartprojectmargie2/security/RegistrationController.java
@@ -6,8 +6,13 @@ import com.metzm.cartprojectmargie2.models.data.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/register")
@@ -25,6 +30,22 @@ public class RegistrationController {
         return "register";
     }
 
+    @PostMapping//register the user
+    public String register (@Valid User user, BindingResult bindingResult, Model model) {
 
+        if (bindingResult.hasErrors()){
+            return "register";
+        }
+        //to confirm passsword
 
-}
+        if (! user.getPassword().equals(user.getConfirmPassword())) {
+            model.addAttribute("passwordMatchProblem","Passwords do not match!");
+            return "register";
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepo.save(user);
+
+         return "redirect:/login";
+    }
+    }
