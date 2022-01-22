a68672e5515e81d93650c1583736637b9c8ae0df
metzmom <84488543+metzmom@users.noreply.github.com>
2022-01-22 00:12:18 -0600

    added bill to login

--- cartprojectmargie2/src/main/java/com/metzm/cartprojectmargie2/security/SecurityConfig.java
+++ cartprojectmargie2/src/main/java/com/metzm/cartprojectmargie2/security/SecurityConfig.java
@@ -1,41 +1,15 @@
 package com.metzm.cartprojectmargie2.security;
 
 
-/*import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-@Service
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-    @Bean
-    public PasswordEncoder encoder()
-
-
-        return new BCryptPasswordEncoder();
-    }
-
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-       http
-               .authorizeRequests()
-
-          //  .antMatchers("/**").hasAnyRole("USER");//allows only home page
-            .antMatchers("/").permitAll();//allows everyone everywhere
-
-    }
-}*/
-import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.context.annotation.Bean;
-        import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-        import org.springframework.security.core.userdetails.UserDetailsService;
-        import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-        import org.springframework.security.crypto.password.PasswordEncoder;
-        import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -48,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Override//looking up data on user
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .userDetailsService(userDetailsService)
@@ -59,19 +33,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-            //   .antMatchers("/category/**").hasAnyRole("USER", "ADMIN")
-
-              .antMatchers("/admin/**").hasAnyRole("ADMIN")
+               // .antMatchers("/category/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/admin/**").hasAnyRole("ADMIN")
                 .antMatchers("/").permitAll()
-                     .and()
-                         .formLogin()
-                         .loginPage("/login")
-                              .and()
-                                 .logout()
-                                 .logoutSuccessUrl("/")
-                              .and()
-                                  .exceptionHandling()
-                                      .accessDeniedPage("/");
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/");
         // .antMatchers("/**").hasAnyRole("USER");
 
         // http
@@ -81,3 +54,57 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 }
+//import org.springframework.beans.factory.annotation.Autowired;
+//        import org.springframework.context.annotation.Bean;
+//        import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//        import org.springframework.security.core.userdetails.UserDetailsService;
+//        import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//        import org.springframework.security.crypto.password.PasswordEncoder;
+//        import org.springframework.stereotype.Service;
+//
+//@Service
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Autowired
+//    private UserDetailsService userDetailsService;
+//
+//    @Bean
+//    public PasswordEncoder encoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override//looking up data on user
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .userDetailsService(userDetailsService)
+//                .passwordEncoder(encoder());
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//            //   .antMatchers("/category/**").hasAnyRole("USER", "ADMIN")
+//                .antMatchers("/pages/**").hasAnyRole( "ADMIN")
+//            //  .antMatchers("/admin/**").hasAnyRole("ADMIN")
+//                .antMatchers("/").permitAll()
+//                     .and()
+//                         .formLogin()
+//                         .loginPage("/login")
+//                              .and()
+//                                 .logout()
+//                                 .logoutSuccessUrl("/")
+//                              .and()
+//                                  .exceptionHandling()
+//                                      .accessDeniedPage("/");
+//        // .antMatchers("/**").hasAnyRole("USER");
+//
+//        // http
+//        //     .authorizeRequests()
+//        //         .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+//        //         .antMatchers("/").access("permitAll");
+//    }
+//
+//}
